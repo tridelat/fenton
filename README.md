b2533436e720b2719251bfb78ad9cf1605eded14
Tristan de Lataillade <delataillade.tristan@gmail.com>
2017-04-05 06:59:18 -0500

    Initial commit - Linux version of Fourier.cpp

---
+++ README.md
@@ -0,0 +1,4 @@
+ # Fenton
+
+Files for getting Fenton waves profile.
+Modified from http://johndfenton.com/ to compile on Linux.
---
+++ testt.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+
+
+int main() {
+	double sqrtAbs(sqrt(2./3.));
+    std::cout << "hello";
+}
